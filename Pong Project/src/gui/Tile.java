93e9e0e718d8fa361ad8a252b0d4d8c9f0b0d2fc
Andew Pulsifer <pulsifera@wit.edu>
2022-04-03 22:06:14 -0400

    testing

---
+++ Pong Project/src/gui/Tile.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.scene.shape.Rectangle;
+
+public class Tile {
+	private Rectangle rect;
+
+	public Tile(int x, int y) {
+		rect = new Rectangle(x, y, 10, 10);
+	}
+	
+	
+}
